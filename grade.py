a91ad0f829bead00a949ee6d4863cb06aa4ed87d
Thavee <94915523+thaveesh@users.noreply.github.com>
2022-01-21 15:22:03 +0530

    Add files via upload

---
+++ grade.py
@@ -0,0 +1,11 @@
+marks=int(input('\033[1;32m'"Enter Your Marks:"))
+if (marks>=75):
+ print ('\033[1;33m'"A Pass")
+elif (marks>=65):
+ print ('\033[1;33m'"B Pasa")
+elif (marks>=55):
+ print ('\033[1;33m'"C Pass")
+elif (marks>=45):
+ print ('\033[1;33m'"D Pass")
+else:
+ print ('\033[1;33m'"Fail")
